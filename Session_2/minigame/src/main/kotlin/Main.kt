9ddf01efe1c64a09ca88f2b776b9197e8d5cb276
markparagas <mark.paragas@student.ehb.be>
2021-10-12 12:03:40 +0200

    challenge one (WIP)

---
+++ Session_2/minigame/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Which country has black, yellow and red flag?");
+    challengeOne()
+}
+
+fun challengeOne(){
+    val correctAnswer = "Belgium"
+    val playerAnwser = readLine();
+    
+}
