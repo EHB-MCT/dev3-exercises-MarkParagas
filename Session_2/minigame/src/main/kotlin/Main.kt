8e34112c6e24a5f2804acce58757191518a812d8
markparagas <mark.paragas@student.ehb.be>
2021-10-12 12:51:15 +0200

    Challenge One (WIP)
    Guess game, guess which it is country.
    Problem with Set of Strings

--- Session_2/minigame/src/main/kotlin/Main.kt
+++ Session_2/minigame/src/main/kotlin/Main.kt
@@ -1,10 +1,29 @@
 fun main(){
     println("Which country has black, yellow and red flag?");
+
     challengeOne()
 }
 
 fun challengeOne(){
-    val correctAnswer = "Belgium"
+    val correctAnswer = setOf<Strings>("Belgium", "German")
     val playerAnwser = readLine();
-    
+    if (playerAnwser == correctAnswer){ // if equal
+        println("Correct answer!")
+        main()
+    }
+    else (playerAnwser != correctAnswer){ // if not equal
+        gameOver()
+    }
+
+}
+
+fun gameOver() {
+    println("Game Over");
+    println("Try Again?");
+    println("Type 'Yes'");
+
+    val playerAnwser = readLine();
+    if (playerAnwser == "Yes"){
+        main()
+    }
 }
