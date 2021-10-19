73cd82404003d24f665765e2405ce694f30d212e
markparagas <mark.paragas@student.ehb.be>
2021-10-19 10:27:53 +0200

    Create Class
    Client Brief, creating class where structure of class from courses. Ex. year
    
    Class course (
     val year: Int
    )

---
+++ Session_3/client_brief/src/main/kotlin/Course.kt
@@ -0,0 +1,12 @@
+class Course (
+    val name: String,
+    val semester: Int,
+    val credits: Int,
+    val description: String,
+    val teacher: String,
+    val year: Int,
+    val module: Int,
+    val required: Boolean,
+) {
+
+}
---
+++ Session_3/client_brief/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val design1 = Course(
+        "design 1",
+        1,
+        5,
+        "a fien course",
+        "Maaike Bueten",
+        1,
+        "Design",
+        true
+
+    )
+}
