f311fb8bb9300caf962c1c41dab2d324d1ed8174
markparagas <mark.paragas@student.ehb.be>
2021-10-19 12:27:13 +0200

    Second Face
    Create 3 classes: Student, Teacher and comedian.
    
    Per class has different function talk gossip, funFact and tellJoke

--- Session_3/chatroulette/src/main/kotlin/Person.kt
+++ Session_3/chatroulette/src/main/kotlin/Person.kt
@@ -1,7 +1,11 @@
-class Person(
-    val name: String
+open class Person(
+    open val name: String
     ) {
     fun introduce() {
         println("Hallo, mijn name is ${name}")
     }
+    open fun talk(){
+
+    }
+
 }
