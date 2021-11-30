27284d8e0d434a4cf5f2275570b92bf120377927
markparagas <mark.paragas@student.ehb.be>
2021-11-30 11:55:51 +0100

    User testing
    If & Else statement
    True or Wrong answers

--- Session_7/duolingo/src/main/kotlin/Duolingo.kt
+++ Session_7/duolingo/src/main/kotlin/Duolingo.kt
@@ -1,9 +1,24 @@
 class Duolingo {
+    // val words: MutableList<Word>
     val words = mutableListOf<Word>(
-        Word("hallo", "hello", "Engels"),
-        Word("hallo", "bonjour","Frans"),
+        Word("hallo", "hello", "engels"),
+        Word("hallo", "bonjour", "frans"),
+        Word("hallo", "hallo", "german"),
+        Word("hallo", "Ola", "spaans"),
+        Word("hallo", "Privet", "Russisch"),
     )
+
     fun play() {
-        words.random()
+        val randomWords = words.random()
+        println(randomWords.original)
+        println("What is the translation? Of")
+        println(randomWords.language)
+
+        val userAnswer = readLine() // Write the answer of the user
+        if (userAnswer == randomWords.translated) {
+            println("Correct")
+        } else {
+            println("Wrong")
+        }
     }
 }
