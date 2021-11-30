8e311388505c8c7925cc471e8521789f01e971c1
markparagas <mark.paragas@student.ehb.be>
2021-11-30 12:33:59 +0100

    Duolingo part 1 (complete)
    Using:
    
    - While()
    - x.isNotEmpty()
    - Take(number)
    - ToMutableSet()
    - If statement / [Else]

--- Session_7/duolingo/src/main/kotlin/Duolingo.kt
+++ Session_7/duolingo/src/main/kotlin/Duolingo.kt
@@ -4,21 +4,28 @@ class Duolingo {
         Word("hallo", "hello", "engels"),
         Word("hallo", "bonjour", "frans"),
         Word("hallo", "hallo", "german"),
-        Word("hallo", "Ola", "spaans"),
-        Word("hallo", "Privet", "Russisch"),
+        Word("hallo", "ola", "spaans"),
+        Word("hallo", "privet", "Russisch"),
     )
 
     fun play() {
-        val randomWords = words.random()
-        println(randomWords.original)
-        println("What is the translation? Of")
-        println(randomWords.language)
+        // val randomWords = words.random()
 
-        val userAnswer = readLine() // Write the answer of the user
-        if (userAnswer == randomWords.translated) {
-            println("Correct")
-        } else {
-            println("Wrong")
+        val currentWords = words.take(5) .toMutableSet()//[Take] It will take five words in the sets
+        // toMutableSet() able to change
+        println(currentWords.count())
+
+        while (currentWords.isNotEmpty()) { // Repeat process = (currentWords.count() > 0 )
+            val selectedWord = currentWords.random() // randomise
+            println("What is the translation of ${selectedWord.original} in ${selectedWord.language}  ") // " ${} "
+
+            val userAnswer = readLine() // Write the answer of the user
+
+            if (userAnswer == selectedWord.translated) {
+                currentWords.remove(selectedWord)// Correct
+            } 
+            println(currentWords.count())
         }
+        println("Congrats, you did it :D")
     }
 }
