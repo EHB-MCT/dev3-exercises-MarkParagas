64ddfbbab5e75f305efafc223d6f26b95626fe06
markparagas <mark.paragas@student.ehb.be>
2021-11-30 12:58:56 +0100

    Duo part 2 (WIP)
    working with constructor

--- Session_7/duolingo/src/main/kotlin/Duolingo.kt
+++ Session_7/duolingo/src/main/kotlin/Duolingo.kt
@@ -1,11 +1,22 @@
-class Duolingo {
+class Duolingo (
+    val roundSize: Int,
+    val language: String
+    ) {
+
+    val difficulty = setOf<Strings>("")
+
     // val words: MutableList<Word>
     val words = mutableListOf<Word>(
-        Word("hallo", "hello", "engels"),
-        Word("hallo", "bonjour", "frans"),
-        Word("hallo", "hallo", "german"),
-        Word("hallo", "ola", "spaans"),
-        Word("hallo", "privet", "Russisch"),
+        Word("hello", "hallo", "en"),
+        Word("goodmorning", "goedemorgen", "en"),
+        Word("you", "jij", "en"),
+        Word("me", "ik", "en"),
+        Word("he", "hij", "en"),
+        Word("hey", "hallo", "ge"),
+        Word("guten morgen", "goedemorgen", "ge"),
+        Word("Sie", "jij", "ge"),
+        Word("mich", "ik", "ge"),
+        Word("er", "hij", "ge"),
     )
 
     fun play() {
@@ -17,13 +28,13 @@ class Duolingo {
 
         while (currentWords.isNotEmpty()) { // Repeat process = (currentWords.count() > 0 )
             val selectedWord = currentWords.random() // randomise
-            println("What is the translation of ${selectedWord.original} in ${selectedWord.language}  ") // " ${} "
+            println("What is the translation of ${selectedWord.original}") // " ${} "
 
             val userAnswer = readLine() // Write the answer of the user
 
             if (userAnswer == selectedWord.translated) {
                 currentWords.remove(selectedWord)// Correct
-            } 
+            }
             println(currentWords.count())
         }
         println("Congrats, you did it :D")
